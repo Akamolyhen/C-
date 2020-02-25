9efe78679432d7dfa630d0f8cb0702138f45de03
Akamolyhen <907446875@qq.com>
2020-02-25 17:32:37 +0800

    2

--- 第一次玩C#/test1/test1/test2.cs
+++ 第一次玩C#/test1/test1/test2.cs
@@ -1,16 +1,14 @@
-﻿class HelloWorld
+﻿class Hello
 {
-    private const string V = "比C++烦";
-
     static void Main()
     {
         string str1, str2, str3;
         //str1=System.Console.ReadLine();
         str3 = System.Console.ReadLine();
-        str1 = V;
+        str1 = "wuwu";
         str2 = "好恶心啊！";
 
-        System.Console.WriteLine("我真的\n好\t烦C#{0}{1}", str2, str1 + str3);
+        System.Console.WriteLine("我真的好烦C#{0}{1}", str2, str1 + str3);
 
     }
 }
