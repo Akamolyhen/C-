afb1c31eb72973e7be80ae6742dc2e66408dd7dc
Akamolyhen <907446875@qq.com>
2020-02-26 07:45:41 +0800

    1

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1,str2,str3;
+            System.Console.Write("你的名字是：");
+            str3=System.Console.ReadLine();
+            str2="程锦昊是：";
+            str1=System.Console.ReadLine();
+            System.Console.WriteLine(str2+str1);
+
+        }
+    }
+}
