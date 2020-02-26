55e3bc7e7ab93dbbc74b83509f382bd450089018
Akamolyhen <907446875@qq.com>
2020-02-27 07:52:33 +0800

    12

--- HelloWorld/test.cs
+++ HelloWorld/test.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿//using System;
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//方法的说明，要求返回void或int；且要么无参，要么接收一个字符串数组
         {
             string str1,str2,str3;
             System.Console.Write("你的名字是：");
