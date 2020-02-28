0aa46fd69e1379b238733d8e8232b14de2a418f8
Akamolyhen <907446875@qq.com>
2020-02-28 11:11:09 +0800

    1

--- HelloWorld/test.cs
+++ HelloWorld/test.cs
@@ -21,7 +21,7 @@ namespace HelloWorld
     }
 }
 */
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_16
+/*namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_16
 {
     public class Program
     {
@@ -44,4 +44,108 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_16
             System.Console.WriteLine($"Your full name is {firstName}{lastName} say："+$"{str1}\t{str2}");
         }
     }
+}*/
+
+</pre><pre name="code" class="csharp">using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace NarcissisticNumber
+{
+    class NarNumber//建立水仙数的类，在Main中调用此类
+    {
+        public void NarcisNumber()
+        {
+            Console.WriteLine("*****************输出100~999的水仙花数***************");
+            Console .Write("\n\t100~999之间的水仙花数为：");
+ 
+            for (int i = 100; i <= 999; i++)
+            {
+                if (judge(i))
+                {
+                    Console.Write("{0} ",i);
+                }
+            }
+            Console.WriteLine("\n\n****************************************************\n\n");
+        }
+ 
+        public void Narcinumber()//判断四位数是否为水仙数的函数
+        {
+            Console.WriteLine("*****************输出1000~9999的水仙花数************");
+            Console.Write("\n\t1000~9999的水仙花数为：");
+ 
+            for (int i = 1000; i <= 9999; i++)
+            {
+                if (Judge(i))
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.WriteLine("\n\n****************************************************\n\n");
+        }
+ 
+        public bool judge(int num)  //判断三位水仙花数的语句
+        {
+            int a, b, c;
+            a = num / 100;
+            b = (num - a * 100) / 10;
+            c = num % 10;
+ 
+            if (num == Recurison(a,3) + Recurison(b,3) + Recurison(c,3))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+ 
+        public bool Judge(int num)      //判断四位数为水仙数的语句
+        {
+            int a, b, c, d;
+            a = num / 1000;
+            b = num % 1000 / 100;
+            c = num % 100 / 10;
+            d = num % 10;
+ 
+            if (num == Recurison(a,4) + Recurison(b,4) + Recurison(c,4) + Recurison(d,4))
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+ 
+        public int Recurison(int num, int n)  //利用递归计算  a^4
+        {
+            if (n == 1)
+            {
+                return num;
+            }
+            else
+            {
+                return num * Recurison(num,n-1);
+            }
+        }
+ 
+    }
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NarNumber a = new NarNumber();//构造新的NarNumber函数
+            //判断三位数的调用的函数类
+            a.NarcisNumber();
+            //判断四位数的调用的函数类
+            a.Narcinumber();
+            Console.ReadKey();//防止程序闪退
+        }
+    }
 }
+
---
+++ 水仙花/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace 水仙花
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int ge, shi, bai,qian;
+         Console.WriteLine("所有三位水仙花数是:");
+         for (int i = 100; i <= 999; i++)
+         {
+            ge = i % 10;
+            shi = i / 10 % 10;
+            bai = i / 100;
+            if (ge * ge * ge + shi * shi * shi + bai * bai * bai == i)
+               Console.Write("  {0}", i);
+         
+         }
+         Console.WriteLine("\n所有四位水仙花数是：");
+         for(int i=1000;i<10000;i++)
+         {
+         	ge=i%10;
+         	shi=i/10%10;
+         	bai=i/100%10;
+         	qian=i/1000;
+         	if(ge*ge*ge*ge+shi*shi*shi*shi+bai*bai*bai*bai+qian*qian*qian*qian==i)
+         		{
+         			Console.Write("  {0}", i);
+         		}
+        }
+       
+    }
+}
+}
