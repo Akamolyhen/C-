934ba7eebab30ace5d7d37b7f65aa5d635fa43a7
Akamolyhen <907446875@qq.com>
2020-02-28 11:59:08 +0800

    121

--- 水仙花/Program.cs
+++ 水仙花/Program.cs
@@ -6,6 +6,7 @@ namespace 水仙花
         static void Main(string[] args)
         {
            int ge, shi, bai,qian;
+           int j=0;
          Console.WriteLine("所有三位水仙花数是:");
          for (int i = 100; i <= 999; i++)
          {
@@ -14,14 +15,15 @@ namespace 水仙花
             bai = i / 100;
             if (ge * ge * ge + shi * shi * shi + bai * bai * bai == i)
                {
-               	Console.Write("  {0}", i);
+               	Console.Write($"  {i}");
+                j++;
 
                }
          
          }
-        // Console.Write($"共有{j}个");
+         Console.WriteLine($"共有{j}个");
          Console.WriteLine("\n所有四位水仙花数是：");
-         //j=0;
+         j=0;
          for(int i=1000;i<10000;i++)
          {
          	ge=i%10;
@@ -30,9 +32,11 @@ namespace 水仙花
          	qian=i/1000;
          	if(ge*ge*ge*ge+shi*shi*shi*shi+bai*bai*bai*bai+qian*qian*qian*qian==i)
          		{
-         			Console.Write("  {0}", i);
+         			Console.Write($"  {i}");
+                    j++;
          		}
         }
+        Console.WriteLine($"共有{j}个");
        
        
     }
