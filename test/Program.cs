2fd3186de63d5ffdbad1d75e754b6a060ffc539b
Akamolyhen <907446875@qq.com>
2020-03-11 19:12:02 +0800

    5

--- test/Program.cs
+++ test/Program.cs
@@ -8,16 +8,7 @@ namespace test
         {
             Console.WriteLine("Hello World!");
             string i;
-            while (true)
-            {
-                i = Console.ReadLine();
-                foreach (char c in i)
-                {
-                    if ((int)c > 0)
-                        break;
-                }
-            }
-            Console.WriteLine("hehe");
+          
             
            
             //int num=Convert.ToInt32(i);
