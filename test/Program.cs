b0ee38e4c5459f668fefea381acd48a492aa31eb
Akamolyhen <907446875@qq.com>
2020-04-28 13:09:03 +0800

    1

---
+++ test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string i;
+          
+            
+           
+            //int num=Convert.ToInt32(i);
+            //Console.WriteLine(num);
+        }
+    }
+}
